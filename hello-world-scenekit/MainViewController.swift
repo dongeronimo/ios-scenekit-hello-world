b4ffbeb16638b1827d72e78737b562a5c0cd652f
geronimo-lisboa <luciano.geronimo.fnord@gmail.com>
2019-09-29 11:54:47 -0300

    Shader custom ok

--- hello-world-scenekit/MainViewController.swift
+++ hello-world-scenekit/MainViewController.swift
@@ -19,22 +19,50 @@ class MainViewController: UIViewController {
         sceneView.backgroundColor = UIColor.black
         sceneView.autoenablesDefaultLighting = true
         sceneView.allowsCameraControl = true
+        testShader(myScene: myScene)
     }
+    private func testShader(myScene: SCNScene){
+        //Search for the shaderBox
+        let shaderBox: SCNNode = myScene.rootNode.childNode(withName: "shader box", recursively: true)!
+        let program: SCNProgram = SCNProgram()//Instancia o objeto de programa
+        program.fragmentFunctionName = "myFragment"
+        program.vertexFunctionName = "myVertex"
+        
+        //.... http://dontpad.com/geronimol
+        shaderBox.geometry!.firstMaterial?.program = program//Passa pro shaderBox
+    }
+    
     @IBAction func onTap(_ sender: UITapGestureRecognizer) {
         let p = sender.location(in: sceneView)
         let hitResults = sceneView.hitTest(p, options: [:])
         if (hitResults.count > 0 ){
             let result = hitResults[0]
-            objectName.text = result.node.name
-            let material = result.node.geometry!.firstMaterial!//Pega o material
-            SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
-            material.diffuse.contents = UIColor(displayP3Red: CGFloat(Double.random),
-                                                green: CGFloat(Double.random),
-                                                blue: CGFloat(Double.random),
-                                                alpha:  1.0)
-            SCNTransaction.commit()
+            let node = result.node
+            objectName.text = node.name
+            if(node.name == "shader box"){
+//                //É no shaderModifiers que eu enfio o meu shader custom.
+//                //Ao tocar no botão vou jogar fora todo o shading e retornar um bagulho pink.
+//                node.geometry!.shaderModifiers = [.fragment:
+//                    """
+//                    _output.color.rgb = float3(1.0, 0.0, 1.0);
+//                    """]
+            }
+            else{
+                changeDiffuseToRandomColor(node: node)
+            }
         }
+    }
+    ///Sets the diffuse channel to some random color, changing from the old color to the
+    ///new color in a half second.
+    private func changeDiffuseToRandomColor(node: SCNNode){
+        let material = node.geometry!.firstMaterial!//Pega o material
+        SCNTransaction.begin()
+        SCNTransaction.animationDuration = 0.5
+        material.diffuse.contents = UIColor(displayP3Red: CGFloat(Double.random),
+                                            green: CGFloat(Double.random),
+                                            blue: CGFloat(Double.random),
+                                            alpha:  1.0)
+        SCNTransaction.commit()
     }
     
     override var shouldAutorotate: Bool {
